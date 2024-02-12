2e7d753ccd353bf5913612b8852fb729ac1cc699
Romulo Santos <romulosnik@hotmail.com>
2024-02-12 17:27:40 -0300

    Populando tabela Categorias usando Migrarions , migrationBuilder

---
+++ Aux_3d/Migrations/20240212195548_PopularCategorias.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aux_3d.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                                 "VALUES('Linha Action Figures','Action Figures em geral animes e estaturas de resinas')");
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                                 "VALUES('Linha Decoração','Decorações em geral e estaturas de resinas')");
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                                 "VALUES('Mais Vendidos','Relação dos itens mais vendidos')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categoria");
+        }
+    }
+}
